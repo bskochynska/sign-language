0fadf3299144d1834f6be7f021b22c126b206338
bskochynska <124739569+bskochynska@users.noreply.github.com>
2025-05-23 17:06:30 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -24,35 +24,4 @@ setLogLevel('debug');
 
 export { firebase, auth, db};
 
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   authDomain: "sign-language-ai-13704.firebaseapp.com",
-//   projectId: "sign-language-ai-13704",
-//   storageBucket: "sign-language-ai-13704.firebasestorage.app",
-//   messagingSenderId: "84907049940",
-//   appId: "1:84907049940:web:dfe6193ca83d9904254c41",
-//   measurementId: "G-7X32PBP220"
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);   - не тре
-
-
-
-
-
-
-
-
-// firebase.initializeApp(firebaseConfig);
-
-// const auth = firebase.auth();
-// const db = firebase.firestore();
-
-// export { firebase, auth, db};
