9366ab221e457f021ac5f4f84676718e471991d4
bskochynska <124739569+bskochynska@users.noreply.github.com>
2025-05-23 17:02:01 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -31,7 +31,6 @@ export { firebase, auth, db};
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // const firebaseConfig = {
-//   apiKey: "AIzaSyCVCIaSQJWy4iEzcl1ozlRtLVIauVzs_QE",
 //   authDomain: "sign-language-ai-13704.firebaseapp.com",
 //   projectId: "sign-language-ai-13704",
 //   storageBucket: "sign-language-ai-13704.firebasestorage.app",
